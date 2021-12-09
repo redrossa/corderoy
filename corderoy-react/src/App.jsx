f4998c8e2e2d1e7c8d067be7675b7151c38f833f
redrossa <adriano.raksi@gmail.com>
2021-12-08 18:12:37 -0600

    Moved settings to client side

--- corderoy-react/src/App.jsx
+++ corderoy-react/src/App.jsx
@@ -5,21 +5,17 @@ import Home from './pages/Home/Home';
 import {Wardrobe, Catalog} from './pages/Wardrobe';
 import {Share} from './pages/Share';
 import useOutfit from './hooks/useOutfit';
-import Trending from "./pages/Home/Trending";
-import Feed from "./pages/Home/Feed";
 import axios from 'axios';
 import {Collections} from './components/Collections';
+import Settings from './data-settings.json';
 
 function App() {
   const outfit = useOutfit();
-  const [collections, setCollections] = useState({});
 
-  useEffect(() => {
-    axios.get('/api/settings')
-        .then(resp => {
-          setCollections(resp.data.tree);
-        })
-  }, [])
+  const collections = {
+    tree: Settings.tree,
+    collectionRank: Settings.collectionRank
+  };
 
   return (
       <div className="App">
@@ -27,10 +23,8 @@ function App() {
           <UserSelection.Provider value={outfit}>
             <BrowserRouter>
               <Routes>
-                <Route path="/" exact element={<Home/>}>
-                  <Route path="" element={<Trending />} />
-                  <Route path="/search" element={<Feed />} />
-                </Route>
+                <Route path="/" exact element={<Home/>} />
+                {/*<Route path="/search" element={<Feed />} />*/}
                 <Route path="/wardrobe" element={<Wardrobe/>}>
                   <Route path=":collection" element={<Catalog/>} />
                 </Route>
